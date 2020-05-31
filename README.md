900ed47981289bf5322761fad437deebc9847f46
James Su-Shum <sushumjames@gmail.com>
2020-05-31 17:20:23 -0500

    exercise files

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript Objects vs Arrays
+
+Exercise on JavaScript objects and arrays.
---
+++ lightningExercises.js
@@ -0,0 +1,18 @@
+// Object vs Array: Lightning Exercise 1
+const ford = {
+  model: "mustang",
+  year: "2015",
+  color: "red"
+}
+
+console.log(ford);
+
+// Object vs Array: Lightning Exercise 2
+const animalNames = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
+
+console.log(animalNames);
+
+// Object vs Array: Lightning Exercise 3
+const familyMembers = [{name: "Jack", age: "21", gender: "male"}, {name: "Jill", age: "40", gender: "female"}, {name: "John", age: "50", gender: "male"}]
+
+console.log(familyMembers);
